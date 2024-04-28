b045745e607e190e3e58ce150c3c46e67be69118
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-04-28 22:11:09 +0600

    impliment get method which to use  spacific id and loaded data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require ('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,13 @@ async function run() {
     app.get('/Tourist_Sports', async (req,res)=>{
         const query = Tourist_Sport_Collection.find();
         const result = await query.toArray();
+        res.send(result);
+    })
+
+    app.get('/Tourist_Sports/:id',async (req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)};
+        const result = await Tourist_Sport_Collection.findOne(query);
         res.send(result);
     })
     app.post('/Tourist_Sports', async (req,res)=>{
