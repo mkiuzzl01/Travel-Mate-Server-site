2097d5e55a85202fc19295f7a6e70b448975d199
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-04-30 20:29:41 +0600

    this backend server is ok and deploy successfully complete

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
     const Tourist_Sport_Collection = client
       .db("Tourist_SportsDB")
       .collection("All_Spots");
-      const UserCollection = client.db("Tourist_SportsDB").collection('Users')
+    const UserCollection = client.db("Tourist_SportsDB").collection("Users");
     const CountriesCollection = client
       .db("Tourist_SportsDB")
       .collection("Countries");
@@ -52,36 +52,36 @@ async function run() {
     app.get("/minCost", async (req, res) => {
       const query = Tourist_Sport_Collection.find();
       const result = await query.toArray();
-      const minData = result.sort(function(a,b){
+      const minData = result.sort(function (a, b) {
         return parseInt(a.average_cost) - parseInt(b.average_cost);
-    })
+      });
       res.send(minData);
     });
 
     app.get("/maxCost", async (req, res) => {
       const query = Tourist_Sport_Collection.find();
       const result = await query.toArray();
-      const minData = result.sort(function(a,b){
+      const minData = result.sort(function (a, b) {
         return parseInt(a.average_cost) - parseInt(b.average_cost);
-    })
-      minData.reverse()
+      });
+      minData.reverse();
       res.send(minData);
     });
 
     app.get("/Tourist_Sports/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await Tourist_Sport_Collection.findOne(query);
       res.send(result);
     });
 
     app.get("/My_List/:email", async (req, res) => {
-        const email = req.params.email;
-        const query = {userEmail:email};
-        const result = await Tourist_Sport_Collection.find(query).toArray();
-        console.log(result);
-        res.send(result);
-      });
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await Tourist_Sport_Collection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
 
     app.post("/Tourist_Sports", async (req, res) => {
       const Sports = req.body;
@@ -95,36 +95,40 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/Tourist_Sports/:id", async(req,res)=>{
-        const id = req.params.id;
-        const filter = {_id:new ObjectId(id)};
-        const option = {upsert:true};
-        const updated = req.body;
-        const sport = {
-            $set:{
-                sport_name:updated.sport_name,
-                country_Name:updated.country_Name,
-                location:updated.location,
-                description:updated.description,
-                average_cost:updated.average_cost,
-                travel_time:updated.travel_time,
-                total_visitor:updated.total_visitor,
-                photo:updated.photo,
-                seasonal:updated.seasonal,
-                userEmail:updated.userEmail,
-                userName:updated.userName,
-            }
-        }
-        const result = await Tourist_Sport_Collection.updateOne(filter,sport,option,);
-        res.send(result);
-    })
+    app.put("/Tourist_Sports/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updated = req.body;
+      const sport = {
+        $set: {
+          sport_name: updated.sport_name,
+          country_Name: updated.country_Name,
+          location: updated.location,
+          description: updated.description,
+          average_cost: updated.average_cost,
+          travel_time: updated.travel_time,
+          total_visitor: updated.total_visitor,
+          photo: updated.photo,
+          seasonal: updated.seasonal,
+          userEmail: updated.userEmail,
+          userName: updated.userName,
+        },
+      };
+      const result = await Tourist_Sport_Collection.updateOne(
+        filter,
+        sport,
+        option
+      );
+      res.send(result);
+    });
 
-    app.delete("/Tourist_Sports/:id", async (req,res)=>{
-        const id = req.params.id;
-        const query = {_id:new ObjectId(id)}
-        const result = await Tourist_Sport_Collection.deleteOne(query);
-        res.send(result);
-    })
+    app.delete("/Tourist_Sports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await Tourist_Sport_Collection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
